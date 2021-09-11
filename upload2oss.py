768252b45f23f73518452ac00a73accd27dbafbe
yingcheng <yingchengpa@qq.com>
2021-09-11 22:26:07 +0800

    Update upload2oss.py
    修改oss账号密码

--- upload2oss.py
+++ upload2oss.py
@@ -10,7 +10,7 @@ def upload2oss_bing(filename, filepath):
     # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
     # return oss url
     bucketname = 'bing-pic'
-    auth = oss2.Auth('LTAI5tEDq9Va5MHKMswUJbRs', '9SPENIDYOgdQlnUBsHRRATAGVSH0QO')
+    auth = oss2.Auth('123123', '123123')
     # yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
     # 填写Bucket名称。
     bucket = oss2.Bucket(auth, 'https://oss-cn-hangzhou.aliyuncs.com', bucketname)
